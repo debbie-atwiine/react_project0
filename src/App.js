9895ae16c52cba87a7a973dc55a8dacce308bed6
debbie_atwiine <deborahatwiine75@gmail.com>
2022-06-24 12:05:06 -0700

    more changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 
 const App = () => {
-  const name = 'Joy';
-  const isNameShowing = false;
-
+  const name = null;
+  
   return (
     <div className="App">
-      <h1>Hello {isNameShowing ? name : 'someone'}</h1>
+      <h1>Hello {name}</h1>
+      {
+        name ? (
+        <>
+          <h1>{name}</h1>
+        </>
+        ) : (
+          <>
+            <h1>test</h1>
+            <h2>There is no name</h2>
+          </>
+        )
+      }
     
     </div>
   );
