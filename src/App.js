164121e7d8133bdaba3e077ae5d0775c90a0a23f
debbie_atwiine <deborahatwiine75@gmail.com>
2022-06-24 12:45:05 -0700

    introducing states

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-      <h1>First Name: {props.fname} </h1>
-      <h3>Last Name: {props.lname}</h3>
-      <h3>Age: {props.age} </h3>
-    </>
-  )
-}
 
 const App = () => {
+  const [counter, setCounter] = useState(0)
+
+
   return (
     <div className="App">
-      <Person 
-        fname='Jane' 
-        lname='Eyre' 
-        age={10 * 2} 
-      />
-      <Person 
-        fname='John' 
-        lname='Doe' 
-        age={10 + 12} 
-      />
-      <Person 
-        fname='Joy' 
-        lname='Reeves' 
-        age={49 / 2} 
-      />
+      <button onClick={ () => setCounter((prevCount) => prevCount - 1) }> - </button> 
+      <button> {counter} </button> 
+      <button onClick={ () => setCounter((prevCount) => prevCount + 1) }> + </button> 
     </div>
   );
 }
