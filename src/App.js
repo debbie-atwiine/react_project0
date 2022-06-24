0ee0a6f444efa49a4cae32833fcb171dcfef0e10
debbie_atwiine <deborahatwiine75@gmail.com>
2022-06-24 16:49:08 -0700

    starting on a new project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-
+import React from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-
-  useEffect( () => {
-    alert('Initial value of the counter has been set to 100');
-    setCounter(100);
-  }, [])
-
-  return (
-    <div className="App">
-      <button onClick={ () => setCounter((prevCount) => prevCount - 1) }> - </button> 
-      <button> {counter} </button> 
-      <button onClick={ () => setCounter((prevCount) => prevCount + 1) }> + </button> 
-    </div>
-  );
+    return (
+        <h1>This is the app</h1>
+    );
 }
 
-export default App;
+export default App
